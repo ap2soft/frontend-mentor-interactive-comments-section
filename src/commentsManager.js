64d7061b2609860b95726fa6c1aac23418e8288d
Andrew Ponomarev <spargo32@gmail.com>
2022-04-17 12:39:04 +0300

    Comments Manager

---
+++ src/commentsManager.js
@@ -0,0 +1,58 @@
+class commentsManager {
+  /**
+   * Get dummy comments from API
+   * - randomize date and likes count
+   * - set dummy user avatar
+   * @param {int} count Count of comments to retrieve
+   * @returns
+   */
+  seed(count = 10) {
+    return fetch(`https://dummyjson.com/comments?limit=${count}`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (!data.comments) return;
+
+        const comments = data.comments.map((comment) => {
+          const date = new Date();
+          date.setHours(date.getHours() - Math.round(Math.random() * 24 * 3));
+
+          return {
+            id: comment.id,
+            replyTo: null,
+            author: {
+              name: comment.user.username,
+              avatar: `https://i.pravatar.cc/150?u=${comment.user.username}`,
+            },
+            body: comment.body,
+            createdAt: date.toJSON(),
+            likesCount: Math.round(Math.random() * 25),
+          };
+        });
+
+        localStorage.setItem("comments", JSON.stringify(comments));
+        localStorage.setItem(
+          "currentUser",
+          JSON.stringify(
+            comments[Math.floor(Math.random() * comments.length)].author
+          )
+        );
+      });
+  }
+
+  getAll() {
+    return JSON.parse(localStorage.getItem("comments") || "[]");
+  }
+
+  getCurrentUser() {
+    return JSON.parse(localStorage.getItem("currentUser") || "{}");
+  }
+
+  send(comment) {
+    localStorage.setItem(
+      "comments",
+      JSON.stringify([...this.getAll(), comment])
+    );
+  }
+}
+
+export default commentsManager;
