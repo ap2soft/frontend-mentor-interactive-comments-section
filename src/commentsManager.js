fc7aae23f4e7a368b985fb58ecf968e38bb52b04
Andrew Ponomarev <spargo32@gmail.com>
2022-04-18 22:57:33 +0300

    Comment Manager - refactor to functions

--- src/commentsManager.js
+++ src/commentsManager.js
@@ -1,65 +1,52 @@
-class commentsManager {
-  /**
-   * Get dummy comments from API
-   * - randomize date and likes count
-   * - set dummy user avatar
-   * @param {int} count Count of comments to retrieve
-   * @returns
-   */
-  seed(count = 10) {
-    return fetch(`https://dummyjson.com/comments?limit=${count}`)
-      .then((response) => response.json())
-      .then((data) => {
-        if (!data.comments) return;
+/**
+ * Get dummy comments from API
+ * - randomize date and likes count
+ * - set dummy user avatar
+ * @param {int} count Count of comments to retrieve
+ * @returns
+ */
+export const reseedComments = async (count = 10) => {
+  const response = await fetch(`https://dummyjson.com/comments?limit=${count}`);
+  const data = response.json();
 
-        const comments = data.comments.map((comment) => {
-          const date = new Date();
-          date.setHours(date.getHours() - Math.round(Math.random() * 24 * 3));
+  if (!data.comments) return;
 
-          return {
-            id: comment.id,
-            replyTo: null,
-            author: {
-              name: comment.user.username,
-              avatar: `https://i.pravatar.cc/150?u=${comment.user.username}`,
-            },
-            body: comment.body,
-            createdAt: date.toJSON(),
-            likesCount: Math.round(Math.random() * 25),
-          };
-        });
+  const comments = data.comments.map((comment) => {
+    const date = new Date();
+    date.setHours(date.getHours() - Math.round(Math.random() * 24 * 3));
 
-        localStorage.setItem("comments", JSON.stringify(comments));
-        localStorage.setItem(
-          "currentUser",
-          JSON.stringify(
-            comments[Math.floor(Math.random() * comments.length)].author
-          )
-        );
-      });
-  }
+    return {
+      id: comment.id,
+      replyTo: null,
+      author: {
+        name: comment.user.username,
+        avatar: `https://i.pravatar.cc/150?u=${comment.user.username}`,
+      },
+      body: comment.body,
+      createdAt: date.toJSON(),
+      likesCount: Math.round(Math.random() * 25),
+    };
+  });
 
-  getAll() {
-    return JSON.parse(localStorage.getItem("comments") || "[]");
-  }
+  localStorage.setItem("comments", JSON.stringify(comments));
+  localStorage.setItem(
+    "currentUser",
+    JSON.stringify(comments[Math.floor(Math.random() * comments.length)].author)
+  );
+};
 
-  getCurrentUser() {
-    return JSON.parse(localStorage.getItem("currentUser") || "{}");
-  }
+export const getAllComments = () => getItem("comments", "[]");
 
-  send(comment) {
-    localStorage.setItem(
-      "comments",
-      JSON.stringify([...this.getAll(), comment])
-    );
-  }
+export const getCurrentUser = () => getItem("currentUser", "{}");
 
-  deleteComment(commentId) {
-    localStorage.setItem(
-      "comments",
-      JSON.stringify(this.getAll().filter(({ id }) => id !== commentId))
-    );
-  }
-}
+export const sendComment = (comment) =>
+  storeComments([...getAllComments(), comment]);
 
-export default commentsManager;
+export const deleteComment = (commentId) =>
+  storeComments(getAllComments().filter(({ id }) => id !== commentId));
+
+const getItem = (name, defaultValue) =>
+  JSON.parse(localStorage.getItem(name) || defaultValue);
+
+const storeComments = (comments) =>
+  localStorage.setItem("comments", JSON.stringify(comments));
