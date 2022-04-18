2f62218f980428ba94443ffd75258dd547e70fc5
Andrew Ponomarev <spargo32@gmail.com>
2022-04-18 22:58:17 +0300

    App.jsx - refactor to function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,61 +1,51 @@
-import React from "react";
-import commentsManager from "../commentsManager";
+import { useState } from "react";
+import {
+  sendComment,
+  deleteComment,
+  getCurrentUser,
+  getAllComments,
+} from "../commentsManager";
 import CommentForm from "./Comment/Form";
 import { List as CommentList } from "./Comment/List";
 import { Seeder as CommentSeeder } from "./Comment/Seeder";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const [comments, setComments] = useState(getAllComments());
+  const currentUser = getCurrentUser();
 
-    this.commentsManager = new commentsManager();
+  const reloadComments = () => setComments(getAllComments());
 
-    this.state = {
-      comments: this.commentsManager.getAll(),
-      currentUser: this.commentsManager.getCurrentUser(),
-    };
-
-    this.onSend = this.onSend.bind(this);
-    this.onDelete = this.onDelete.bind(this);
-  }
-
-  reloadComments() {
-    this.setState({ comments: this.commentsManager.getAll() });
-  }
-
-  onSend({ comment }) {
-    this.commentsManager.send({
-      author: this.state.currentUser,
+  const sendHandler = ({ comment }) => {
+    sendComment({
+      author: currentUser,
       body: comment,
       likesCount: 0,
       id: Date.now(),
       createdAt: new Date(),
     });
 
-    this.reloadComments();
-  }
+    reloadComments();
+  };
 
-  onDelete(commentId) {
-    this.commentsManager.deleteComment(commentId);
+  const deleteHandler = (commentId) => {
+    deleteComment(commentId);
 
-    this.reloadComments();
-  }
+    reloadComments();
+  };
 
-  render() {
-    return (
-      <div className="mx-auto max-w-2xl">
-        <CommentList
-          comments={this.state.comments}
-          currentUser={this.state.currentUser}
-          onDeleteComment={this.onDelete}
-        />
+  return (
+    <div className="mx-auto max-w-2xl">
+      <CommentList
+        comments={comments}
+        currentUser={currentUser}
+        onDeleteComment={deleteHandler}
+      />
 
-        <CommentForm className="mt-6" onSend={this.onSend} />
+      <CommentForm className="mt-6" onSend={sendHandler} />
 
-        <CommentSeeder className="mt-6 border-y border-blue-light py-4" />
-      </div>
-    );
-  }
-}
+      <CommentSeeder className="mt-6 border-y border-blue-light py-4" />
+    </div>
+  );
+};
 
 export default App;
