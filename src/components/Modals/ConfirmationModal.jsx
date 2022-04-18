5e1913615e38afdc1147ac2a429e94f60149dd5d
Andrew Ponomarev <spargo32@gmail.com>
2022-04-18 23:25:46 +0300

    Confirmation Modal: process Esc, Enter

--- src/components/Modals/ConfirmationModal.jsx
+++ src/components/Modals/ConfirmationModal.jsx
@@ -1,3 +1,5 @@
+import { useCallback, useEffect } from "react";
+
 const ConfirmationModal = ({
   show,
   title,
@@ -6,35 +8,58 @@ const ConfirmationModal = ({
   confirmButtonText,
   onCancel,
   onConfirm,
-}) => (
-  <div
-    className={`fixed inset-0 bg-gray-dark/60 ${!show && "hidden"} transition`}
-  >
-    <div className="grid h-full w-full place-content-center">
-      <div className="m-4 max-w-sm rounded-md bg-white p-8">
-        <header className="">
-          <h1 className="text-lg font-bold text-gray-dark">
-            {title || Confirmation}
-          </h1>
-        </header>
-        {body && <main className="mt-4 tracking-wide">{body}</main>}
-        <footer className="mt-4 flex items-center justify-between">
-          <button
-            className="rounded-md bg-gray px-4 py-2 font-bold uppercase text-white"
-            onClick={onCancel}
-          >
-            {cancelButtonText || "Cancel"}
-          </button>
-          <button
-            className="rounded-md bg-red px-4 py-2 font-bold uppercase text-white"
-            onClick={onConfirm}
-          >
-            {confirmButtonText || "Confirm"}
-          </button>
-        </footer>
+}) => {
+  const escapeKeyHandler = () => onCancel();
+  const enterKeyHandler = () => onConfirm();
+
+  const keyupHandler = useCallback((event) => {
+    if (event.key === "Escape") {
+      escapeKeyHandler();
+    }
+
+    if (event.key === "Enter") {
+      enterKeyHandler();
+    }
+  });
+
+  useEffect(() => {
+    document.addEventListener("keyup", keyupHandler, false);
+
+    return () => document.removeEventListener("keyup", keyupHandler, false);
+  });
+
+  return (
+    <div
+      className={`fixed inset-0 bg-gray-dark/60 ${
+        !show && "hidden"
+      } transition`}
+    >
+      <div className="grid h-full w-full place-content-center">
+        <div className="m-4 max-w-sm rounded-md bg-white p-8">
+          <header className="">
+            <h1 className="text-lg font-bold text-gray-dark">
+              {title || Confirmation}
+            </h1>
+          </header>
+          {body && <main className="mt-4 tracking-wide">{body}</main>}
+          <footer className="mt-4 flex items-center justify-between">
+            <button
+              className="rounded-md bg-gray px-6 py-2 font-bold uppercase text-white"
+              onClick={onCancel}
+            >
+              {cancelButtonText || "Cancel"}
+            </button>
+            <button
+              className="rounded-md bg-red px-6 py-2 font-bold uppercase text-white"
+              onClick={onConfirm}
+            >
+              {confirmButtonText || "Confirm"}
+            </button>
+          </footer>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ConfirmationModal;
