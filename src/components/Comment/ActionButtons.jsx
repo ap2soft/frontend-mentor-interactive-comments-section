4f024e15b202780b4011e94df8a2e1712bc03c16
Andrew Ponomarev <spargo32@gmail.com>
2022-05-07 12:34:47 +0300

    💅🏻 Refactoring: ActionButons

--- src/components/Comment/ActionButtons.jsx
+++ src/components/Comment/ActionButtons.jsx
@@ -1,49 +1,68 @@
 import { IconDelete, IconEdit, IconReply } from "../Icons";
 
+const DeleteAndEditButtons = ({ deleteHandler, editHandler }) => {
+  return (
+    <div className="flex gap-4">
+      <DeleteButton onClick={deleteHandler} />
+      <EditButton onClick={editHandler} />
+    </div>
+  );
+};
+
+const DeleteButton = ({ onClick }) => {
+  return (
+    <button
+      className="flex items-center gap-2 font-bold text-red transition hover:text-red-light"
+      onClick={onClick}
+    >
+      <IconDelete className="h-3 w-3" />
+      <span>Delete</span>
+    </button>
+  );
+};
+
+const EditButton = ({ onClick }) => {
+  return (
+    <button
+      className="flex items-center gap-2 font-bold text-blue transition hover:text-blue-light"
+      onClick={onClick}
+    >
+      <IconEdit className="h-3 w-3" />
+      <span>Edit</span>
+    </button>
+  );
+};
+
+const ReplyButton = ({ onClick }) => {
+  return (
+    <button
+      className="flex items-center gap-2 font-bold text-blue transition hover:text-blue-light"
+      onClick={onClick}
+    >
+      <IconReply className="h-3 w-3" />
+      <span>Reply</span>
+    </button>
+  );
+};
+
 const ActionButtons = ({
   canManage,
   editHandler,
   replyHandler,
   deleteHandler,
 }) => {
-  return (
-    <div className="flex">
-      {(() => {
-        if (canManage) {
-          // Delete & Edit buttons
-          return (
-            <div className="flex gap-4">
-              <button
-                className="flex items-center gap-2 font-bold text-red transition hover:text-red-light"
-                onClick={deleteHandler}
-              >
-                <IconDelete className="h-3 w-3" />
-                <span>Delete</span>
-              </button>
-              <button
-                className="flex items-center gap-2 font-bold text-blue transition hover:text-blue-light"
-                onClick={editHandler}
-              >
-                <IconEdit className="h-3 w-3" />
-                <span>Edit</span>
-              </button>
-            </div>
-          );
-        }
-
-        // Reply button
-        return (
-          <button
-            className="flex items-center gap-2 font-bold text-blue transition hover:text-blue-light"
-            onClick={replyHandler}
-          >
-            <IconReply className="h-3 w-3" />
-            <span>Reply</span>
-          </button>
-        );
-      })()}
-    </div>
-  );
+  if (canManage) {
+    return (
+      <div className="flex">
+        <DeleteAndEditButtons
+          deleteHandler={deleteHandler}
+          editHandler={editHandler}
+        />
+      </div>
+    );
+  } else {
+    return <ReplyButton onClick={replyHandler} />;
+  }
 };
 
 export default ActionButtons;
